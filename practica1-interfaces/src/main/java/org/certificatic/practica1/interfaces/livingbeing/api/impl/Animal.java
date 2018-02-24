b1fca506f32895e9a1c66b47198b719ff4ab4086
German <german_1241@hotmail.com>
2018-02-24 15:50:04 -0600

    Clase 1 Practica Interfaces

--- practica1-interfaces/src/main/java/org/certificatic/practica1/interfaces/livingbeing/api/impl/Animal.java
+++ practica1-interfaces/src/main/java/org/certificatic/practica1/interfaces/livingbeing/api/impl/Animal.java
@@ -1,9 +1,15 @@
 package org.certificatic.practica1.interfaces.livingbeing.api.impl;
 
-import lombok.Data;
+import org.certificatic.practica1.interfaces.livingbeing.api.ILivingBeing;
 
-@Data
-public class Animal {
-	private String type = "Animal";
-	private String subType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+public abstract class Animal implements ILivingBeing {
+	private @Getter @Setter String type = "Animal";
+	private @Getter @Setter String subType;
 }
--- practica1-interfaces/src/test/java/org/certificatic/practica1/interfaces/livingbeing/test/LivingBeingTest.java
+++ practica1-interfaces/src/test/java/org/certificatic/practica1/interfaces/livingbeing/test/LivingBeingTest.java
@@ -1,5 +1,13 @@
 package org.certificatic.practica1.interfaces.livingbeing.test;
 
+import org.certificatic.practica1.interfaces.livingbeing.api.IBugEater;
+import org.certificatic.practica1.interfaces.livingbeing.api.ILivingBeing;
+import org.certificatic.practica1.interfaces.livingbeing.api.impl.Aardvark;
+import org.certificatic.practica1.interfaces.livingbeing.api.impl.Animal;
+import org.certificatic.practica1.interfaces.livingbeing.api.impl.Plant;
+import org.certificatic.practica1.interfaces.livingbeing.api.impl.VenusFlyTrap;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +17,43 @@ public class LivingBeingTest {
 
 	@Test
 	public void livingBeingTest() {
-		log.info("livingBeingTest -------------------");
 
-		// Implementar
+		// log.info("livingBeingTest -------------------");
+
+		ILivingBeing livingBeing1 = getLivingBeing(true);
+		ILivingBeing livingBeing2 = getLivingBeing(false);
+		Assert.assertNotNull(livingBeing1);
+		Assert.assertNotNull(livingBeing2);
+
+		// log.info("LivingBeing1: {}", livingBeing1);
+		// log.info("LivingBeing2: {}", livingBeing2);
+
+		// livingBeing1.born();
+		// livingBeing1.grow();
+		// livingBeing1.breed();
+		// livingBeing1.die();
+		// ((IBugEater) livingBeing1).eatBug();
+		//
+		// livingBeing2.born();
+		// livingBeing2.grow();
+		// livingBeing2.breed();
+		// livingBeing2.die();
+		// ((IBugEater) livingBeing2).eatBug();
+
+		Assert.assertThat(VenusFlyTrap.class.getSimpleName(), CoreMatchers.is(livingBeing1.getClass().getSimpleName()));
+		Assert.assertTrue(livingBeing1 instanceof VenusFlyTrap);
+		Assert.assertTrue(livingBeing1 instanceof Plant);
+		Assert.assertTrue(livingBeing1 instanceof IBugEater);
+		
+		Assert.assertThat(Aardvark.class.getSimpleName(), CoreMatchers.is(livingBeing2.getClass().getSimpleName()));
+		Assert.assertTrue(livingBeing2 instanceof Aardvark);
+		Assert.assertTrue(livingBeing2 instanceof Animal);
+		Assert.assertTrue(livingBeing2 instanceof IBugEater);
+
+	}
+
+	private ILivingBeing getLivingBeing(boolean isPlanta) {
+		return isPlanta ? new VenusFlyTrap("Planta Carnivora") : new Aardvark("Oso Hormiguero");
 	}
 
 }
