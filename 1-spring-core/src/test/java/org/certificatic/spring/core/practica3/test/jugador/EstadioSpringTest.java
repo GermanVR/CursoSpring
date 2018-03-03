619e18350209fdde69023b8e55ca2e9d9681f508
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-03-03 16:00:45 -0600

    Clase 2 . Parte 2

--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica3/test/jugador/EstadioSpringTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica3/test/jugador/EstadioSpringTest.java
@@ -1,6 +1,7 @@
 package org.certificatic.spring.core.practica3.test.jugador;
 
 import org.certificatic.spring.core.practica3.jugador.JugadorFutbol;
+import org.certificatic.spring.core.practica3.jugador.api.IJugador;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -28,7 +29,7 @@ public class EstadioSpringTest {
 		log.info("estadioSpringTest1 -------------------");
 
 		// Implementar
-		JugadorFutbol jugador = applicationContext.getBean("jugadorBean", JugadorFutbol.class);
+		JugadorFutbol jugador = applicationContext.getBean("jugadorFutbolBean", JugadorFutbol.class);
 
 		Assert.assertNotNull(jugador);
 		Assert.assertNotNull(jugador.getPartido());
@@ -47,6 +48,18 @@ public class EstadioSpringTest {
 		log.info("estadioSpringTest2 -------------------");
 
 		// Implementar
+		IJugador jugador = applicationContext.getBean("jugadorBean", IJugador.class);
+
+		Assert.assertNotNull(jugador);
+		Assert.assertNotNull(jugador.getPartido());
+		Assert.assertNotNull(jugador.getTorneo());
+		Assert.assertNotNull(jugador.getTorneo().getEvento());
+
+		jugador.saludar();
+		
+		log.info("{}", jugador.getPartido());
+		log.info("{}", jugador.getTorneo());
+		log.info("{}", jugador.getTorneo().getEvento());
 	}
 
 }
--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica5/test/scopes/ScopesSpringTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica5/test/scopes/ScopesSpringTest.java
@@ -1,8 +1,11 @@
 package org.certificatic.spring.core.practica5.test.scopes;
 
+import org.certificatic.spring.core.practica5.scopes.bean.Persona;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,6 +17,9 @@ public class ScopesSpringTest {
 	@BeforeClass
 	public static void beforeClass() {
 		// Instanciar ApplicationContext
+		String ruta ="spring/practica5/scopes-application-context.xml";
+		applicationContext = new ClassPathXmlApplicationContext(ruta);
+		System.out.println(" --- ya estoy listo ----");
 	}
 
 	@Test
@@ -22,6 +28,14 @@ public class ScopesSpringTest {
 		log.info("scopeSingletonSpringTest -------------------");
 
 		// Implementar
+		for (int i = 0; i < 3; i++) {
+			Persona persona = applicationContext.getBean("personaSingletonBean",
+					Persona.class);
+			
+			Assert.assertNotNull(persona);
+			log.info("persona: {} {} {}", persona, persona.hashCode(),
+					System.identityHashCode(persona));
+		}
 	}
 
 	@Test
@@ -30,6 +44,17 @@ public class ScopesSpringTest {
 		log.info("scopePrototypeSpringTest -------------------");
 
 		// Implementar
+		for (int i = 0; i < 3; i++) {
+			log.info(" -- pidiendo prototype");
+			
+			Persona persona = applicationContext.getBean("personaPrototypeBean",
+					Persona.class);
 
+			log.info(" -- prototype entregado");
+			
+			Assert.assertNotNull(persona);
+			log.info("persona: {} {} {}", persona, persona.hashCode(),
+					System.identityHashCode(persona));
+		}
 	}
 }
