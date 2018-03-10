6c878e1335286a98e89faf890c664ba82079cb3b
German <german_1241@hotmail.com>
2018-03-10 13:25:03 -0600

    Practica 9 Clase 3

--- 1-spring-core/src/test/java/org/certificatic/spring/core/practica9/test/beandefinheritance/bean/BeanDefinitionInheritanceTest.java
+++ 1-spring-core/src/test/java/org/certificatic/spring/core/practica9/test/beandefinheritance/bean/BeanDefinitionInheritanceTest.java
@@ -1,6 +1,11 @@
 package org.certificatic.spring.core.practica9.test.beandefinheritance.bean;
 
+import org.certificatic.spring.core.practica9.beandefinitioninheritance.bean.ConnectionDataBase;
+import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,7 +16,17 @@ public class BeanDefinitionInheritanceTest {
 	public void beanDefinitionInheritanceTest1() {
 
 		log.info("beanDefinitionInheritanceTest1 -------------------");
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"spring/practica9/bean-def-inheritance-application-context.xml");
 
+		ConnectionDataBase prod = applicationContext.getBean("connectionProdBean", ConnectionDataBase.class);
+		ConnectionDataBase desa = applicationContext.getBean("connectionTestBean", ConnectionDataBase.class);
+
+		Assert.assertNotNull(prod);
+		Assert.assertEquals("Produccion", prod.getDatabase());
+		Assert.assertNotNull(desa);
+		Assert.assertEquals("Desarrllo", desa.getDatabase());
+		((AbstractApplicationContext) applicationContext).close();
 		// Implementar
 	}
 
