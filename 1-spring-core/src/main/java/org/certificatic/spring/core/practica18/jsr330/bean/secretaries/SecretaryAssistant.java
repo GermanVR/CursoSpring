34de5b78bdf399f31b2b56d3e53c3df9bdf91fad
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-03-17 16:17:29 -0600

    Clase 4 - Parte 2

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica18/jsr330/bean/secretaries/SecretaryAssistant.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica18/jsr330/bean/secretaries/SecretaryAssistant.java
@@ -1,10 +1,19 @@
 package org.certificatic.spring.core.practica18.jsr330.bean.secretaries;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.certificatic.spring.core.practica18.jsr330.bean.Employee;
 import org.certificatic.spring.core.practica18.jsr330.bean.Secretary;
+import org.certificatic.spring.core.practica18.jsr330.qualifiers.EmployeeOfSecretaryAssistantQualifier;
+import org.certificatic.spring.core.practica18.jsr330.qualifiers.SecretaryAssistantQualifier;
 
+@Named
+@SecretaryAssistantQualifier
 public class SecretaryAssistant extends Secretary {
 
+	@Inject
+	@EmployeeOfSecretaryAssistantQualifier
 	@Override
 	public void setEmployee(Employee employee) {
 		super.setEmployee(employee);
