db89502399b2333757b03919d70a1c30f40cf8f0
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-05-12 12:05:25 -0500

    Clase 8 - PArte 2

--- 3-spring-data-jdbc/src/main/java/org/certificatic/spring/jdbc/pratica25/dao/springjdbc/rowmapper/AccountRowMapper.java
+++ 3-spring-data-jdbc/src/main/java/org/certificatic/spring/jdbc/pratica25/dao/springjdbc/rowmapper/AccountRowMapper.java
@@ -4,6 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.certificatic.spring.jdbc.pratica25.domain.entities.Account;
+import org.certificatic.spring.jdbc.pratica25.domain.entities.Customer;
+import org.certificatic.spring.jdbc.pratica25.domain.vo.CustomDate;
 import org.springframework.jdbc.core.RowMapper;
 
 public class AccountRowMapper implements RowMapper<Account> {
@@ -13,6 +15,13 @@ public class AccountRowMapper implements RowMapper<Account> {
 		Account account = new Account();
 
 		// Implementar Account row mapper
+		account.setCustomer(new Customer());
+		account.getCustomer().setId(rs.getLong("FK_CUSTOMER_ID"));
+
+		account.setId(rs.getLong("ACCOUNT_ID"));
+		account.setAccountNumber(rs.getString("ACCOUNT_NUMBER"));
+		account.setCreatedDate(new CustomDate(rs.getDate("CREATED_DATE").getTime()));
+		account.setBalance(rs.getBigDecimal("BALANCE"));
 
 		return account;
 	}
