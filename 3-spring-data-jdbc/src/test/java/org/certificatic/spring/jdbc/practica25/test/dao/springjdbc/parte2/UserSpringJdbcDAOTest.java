b015336b7e1eaad5043be5088aa9b42e54a9a142
Ivan Garcia <isc.ivgarcia@gmail.com>
2018-05-12 11:20:54 -0500

    Clase 8 - PArte 1

--- 3-spring-data-jdbc/src/test/java/org/certificatic/spring/jdbc/practica25/test/dao/springjdbc/parte2/UserSpringJdbcDAOTest.java
+++ 3-spring-data-jdbc/src/test/java/org/certificatic/spring/jdbc/practica25/test/dao/springjdbc/parte2/UserSpringJdbcDAOTest.java
@@ -2,7 +2,6 @@ package org.certificatic.spring.jdbc.practica25.test.dao.springjdbc.parte2;
 
 import java.util.List;
 
-import org.certificatic.spring.jdbc.pratica25.dao.api.IAccountDAO;
 import org.certificatic.spring.jdbc.pratica25.dao.api.ICustomerDAO;
 import org.certificatic.spring.jdbc.pratica25.dao.api.IUserDAO;
 import org.certificatic.spring.jdbc.pratica25.domain.entities.Customer;
@@ -20,36 +19,34 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-		locations = "classpath:/spring/practica25/spring-jdbc-application-context.xml")
+@ContextConfiguration(locations = "classpath:/spring/practica25/spring-jdbc-application-context.xml")
 @ActiveProfiles("h2-in-memory")
 public class UserSpringJdbcDAOTest {
 
 	@Autowired
 	private IUserDAO userDAO;
 
-	/*@Autowired
+	@Autowired
 	private ICustomerDAO customerDAO;
 
-	@Autowired
-	private IAccountDAO accountDAO;*/
+	/*
+	 * @Autowired private IAccountDAO accountDAO;
+	 */
 
 	@Before
 	public void setUp() {
 		Assert.assertNotNull(userDAO);
-		//Assert.assertNotNull(customerDAO);
-		//Assert.assertNotNull(accountDAO);
+		Assert.assertNotNull(customerDAO);
+		// Assert.assertNotNull(accountDAO);
 	}
 
 	@Test
 	public void createUserTest() {
 		log.info("createUserTest -------------------");
 
-		User newUser = User.builder().username("laura123").password("123123")
-				.build();
+		User newUser = User.builder().username("laura123").password("123123").build();
 
-		Customer newCustomer = Customer.builder().name("Laura")
-				.lastName("Montes").user(newUser).build();
+		Customer newCustomer = Customer.builder().name("Laura").lastName("Montes").user(newUser).build();
 
 		newUser.setCustomer(newCustomer);
 
@@ -57,8 +54,7 @@ public class UserSpringJdbcDAOTest {
 
 		log.info("newUser : {} {}", newUser, System.identityHashCode(newUser));
 
-		log.info("newUser (detached) : {} {}", newUser,
-				System.identityHashCode(newUser));
+		log.info("newUser (detached) : {} {}", newUser, System.identityHashCode(newUser));
 
 		User user = userDAO.findById(newUser.getId());
 
@@ -66,11 +62,11 @@ public class UserSpringJdbcDAOTest {
 
 		log.info("user : {} {}", user, System.identityHashCode(user));
 
-		//Customer customer = customerDAO.findById(user.getCustomer().getId());
+		Customer customer = customerDAO.findById(user.getCustomer().getId());
 
-		//Assert.assertEquals(user.getCustomer(), customer);
+		Assert.assertEquals(user.getCustomer(), customer);
 
-		//log.info("customer : {} {}", customer, System.identityHashCode(customer));
+		log.info("customer : {} {}", customer, System.identityHashCode(customer));
 
 	}
 
@@ -78,11 +74,9 @@ public class UserSpringJdbcDAOTest {
 	public void updateUserTest() {
 		log.info("updateUserTest -------------------");
 
-		User newUser = User.builder().username("laura123").password("123123")
-				.build();
+		User newUser = User.builder().username("laura123").password("123123").build();
 
-		Customer newCustomer = Customer.builder().name("Laura")
-				.lastName("Montes").user(newUser).build();
+		Customer newCustomer = Customer.builder().name("Laura").lastName("Montes").user(newUser).build();
 
 		newUser.setCustomer(newCustomer);
 
@@ -90,8 +84,7 @@ public class UserSpringJdbcDAOTest {
 
 		log.info("newUser : {} {}", newUser, System.identityHashCode(newUser));
 
-		log.info("newUser (detached) : {} {}", newUser,
-				System.identityHashCode(newUser));
+		log.info("newUser (detached) : {} {}", newUser, System.identityHashCode(newUser));
 
 		User user = userDAO.findById(newUser.getId());
 
@@ -107,32 +100,28 @@ public class UserSpringJdbcDAOTest {
 
 		userDAO.update(user);
 
-		log.info("user (modified) : {} {}", user,
-				System.identityHashCode(user));
+		log.info("user (modified) : {} {}", user, System.identityHashCode(user));
 
 		User modifiedUser = userDAO.findById(user.getId());
 
 		Assert.assertEquals(modifiedUser, user);
 
-		log.info("modifiedUser : {} {}", modifiedUser,
-				System.identityHashCode(modifiedUser));
+		log.info("modifiedUser : {} {}", modifiedUser, System.identityHashCode(modifiedUser));
 
-		//Customer customer = customerDAO.findById(modifiedUser.getCustomer().getId());
+		Customer customer = customerDAO.findById(modifiedUser.getCustomer().getId());
 
-		//Assert.assertEquals(customer, modifiedUser.getCustomer());
+		Assert.assertEquals(customer, modifiedUser.getCustomer());
 
-		//log.info("customer : {} {}", customer, System.identityHashCode(customer));
+		log.info("customer : {} {}", customer, System.identityHashCode(customer));
 	}
 
 	@Test
 	public void deleteUserTest() {
 		log.info("deleteUserTest -------------------");
 
-		User newUser = User.builder().username("laura123").password("123123")
-				.build();
+		User newUser = User.builder().username("laura123").password("123123").build();
 
-		Customer newCustomer = Customer.builder().name("Laura")
-				.lastName("Montes").user(newUser).build();
+		Customer newCustomer = Customer.builder().name("Laura").lastName("Montes").user(newUser).build();
 
 		newUser.setCustomer(newCustomer);
 
@@ -140,8 +129,7 @@ public class UserSpringJdbcDAOTest {
 
 		log.info("newUser : {} {}", newUser, System.identityHashCode(newUser));
 
-		log.info("newUser (detached) : {} {}", newUser,
-				System.identityHashCode(newUser));
+		log.info("newUser (detached) : {} {}", newUser, System.identityHashCode(newUser));
 
 		User user = userDAO.findById(newUser.getId());
 
@@ -157,14 +145,13 @@ public class UserSpringJdbcDAOTest {
 
 		Assert.assertNull(deletedUser);
 
-		log.info("deletedUser : {} {}", deletedUser,
-				System.identityHashCode(deletedUser));
+		log.info("deletedUser : {} {}", deletedUser, System.identityHashCode(deletedUser));
 
-		//Customer customer = customerDAO.findById(user.getCustomer().getId());
+		Customer customer = customerDAO.findById(user.getCustomer().getId());
 
-		//Assert.assertNull(customer);
+		Assert.assertNull(customer);
 
-		//log.info("customer : {} {}", customer, System.identityHashCode(customer));
+		log.info("customer : {} {}", customer, System.identityHashCode(customer));
 	}
 
 	@Test
